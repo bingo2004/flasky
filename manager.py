7cd7aed5e4b528becc40ea5a5f56b283b96804e8
bingo2004 <binbinzhang2008@gmail.com>
2018-05-11 13:42:35 +0800

    x

--- manager.py
+++ manager.py
@@ -1,4 +1,14 @@
 #encoding='utf-8'
+import os
+'''
+if os.path.exists('.env'):
+    print('Importing environment from .env...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+            '''
+
 from flasky import make_shell_context, test, app, migrate
 from flask_script import Manager,Shell
 from flask_migrate import MigrateCommand
@@ -6,4 +16,5 @@ from flask_migrate import MigrateCommand
 manager = Manager(app)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
-manager.run()
+if __name__ == '__main__':
+    manager.run()
